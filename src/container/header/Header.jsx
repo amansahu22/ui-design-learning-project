b9d094792331a719e2bb71106e584e1e3d671641
Aman Sahu <amansahu2019@iiitkottayam.ac.in>
2021-12-02 17:53:54 +0530

    Header Completed

--- src/container/header/Header.jsx
+++ src/container/header/Header.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
-import './header.css'
+import './header.css';
+import people from '../../assests/people.png';
+import ai from '../../assests/ai.png'
 const Header = () => {
     return (
-        <div>
-            Header
+        <div className='gpt3__header section__padding' id='home'>
+            <div className='gpt3__header-content'>
+                <h1 className='gradient__text'>
+                    Let's Build Something amazing with GPT-3 OpenAI
+                </h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Molestie nunc non blandit massa enim nec dui nunc mattis.</p>
+                <div className='gpt3__header-content__input'>
+                    <input type='email' placeholder='Your Email Address' />
+                    <button type='button'>Get Started</button>
+                </div>
+                <div className='gpt3__header-content__people'>
+                    <img src={people} alt='People' />
+                    <p>2000+ people requested access a visit in last 24 hours </p>
+                </div>
+            </div>
+            <div className='gpt3__header-image'>
+                <img src={ai} alt='ai' />
+            </div>
+
         </div>
     )
 }
