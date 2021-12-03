4778cfb86556968a890ce6934496b6ec7c2f15f4
Aman Sahu <amansahu2019@iiitkottayam.ac.in>
2021-12-04 01:23:52 +0530

    Brand Component Finished

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 import './feature.css'
 const Feature = () => {
     return (
-        <div>
-            Feature
+        <div className='gpt3__features-container__feature'>
+            <div className='gpt3__features-container__feature-title'>
+
+            </div>
         </div>
     )
 }
