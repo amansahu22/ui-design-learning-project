527da9dd701d0b0fe6d332e05c2892740ff6be18
Aman Sahu <amansahu2019@iiitkottayam.ac.in>
2021-12-04 01:56:44 +0530

    End of day 2

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import './feature.css'
-const Feature = () => {
+const Feature = ({ title, text }) => {
     return (
         <div className='gpt3__features-container__feature'>
             <div className='gpt3__features-container__feature-title'>
-
+                <div />
+                {/* i will use this empty div to make bar from it */}
+                <h1>{title}</h1>
+            </div>
+            <div className='gpt3__features-container__feature-text'>
+                <p>{text}</p>
             </div>
         </div>
     )
--- src/container/whatGPT3/WhatGPT3.jsx
+++ src/container/whatGPT3/WhatGPT3.jsx
@@ -3,9 +3,9 @@ import { Feature } from '../../components'
 import './whatGPT3.css'
 const WhatGPT3 = () => {
     return (
-        <div className='gpt3__whatgpt3 section__margin'>
+        <div className='gpt3__whatgpt3 section__margin' id='wgpt3'>
             <div className='gpt3__whatgpt3-feature'>
-                <Feature />
+                <Feature title='What Is GPT-3' text='Artificial intelligence is the simulation of human intelligence processes by machines, especially computer systems. Specific applications of AI include expert systems, natural language processing, speech recognition and machine vision.' />
             </div>
             <div className='gpt3__whatgpt3-heading'>
                 <h1 className='gradient__text'>
@@ -14,9 +14,9 @@ const WhatGPT3 = () => {
                 <p>Explore the library</p>
             </div>
             <div className='gpt3__whatgpt3-container'>
-                <Feature />
-                <Feature />
-                <Feature />
+                <Feature title='Chatbots' text='Artificial intelligence is the simulation of human intelligence processes by machines, especially computer systems. Specific applications of AI include expert systems, natural language processing, speech recognition and machine vision.' />
+                <Feature title='Knowledgebase' text='Artificial intelligence is the simulation of human intelligence processes by machines, especially computer systems. Specific applications of AI include expert systems, natural language processing, speech recognition and machine vision.' />
+                <Feature title='Education' text='Artificial intelligence is the simulation of human intelligence processes by machines, especially computer systems. Specific applications of AI include expert systems, natural language processing, speech recognition and machine vision.' />
             </div>
         </div>
     )
